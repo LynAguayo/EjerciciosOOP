11f147e0281458180a987205896d49d41d30f554
evelnyit <125038025+evelnyit@users.noreply.github.com>
2025-02-07 10:19:43 +0100

    Ejercicio 1

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Bienvenido!!");
+
+        // Ejercicio 1
+        Quotation firstQuotation = new Quotation("Welcome to Oracle, the new home of Java!","Oracle Corporation");
+        firstQuotation.display(); // display de attr
+        firstQuotation.setAuthor("Evelyn Aguayo"); // set new author
+        firstQuotation.display();
+
+        // Ejercicio 2
+
+    }
+}
---
+++ src/Quotation.java
@@ -0,0 +1,34 @@
+public class Quotation {
+
+    // Atributos
+    private String quote;
+    private String author;
+
+    // Constructor
+    public Quotation(String quote, String author) {
+        this.quote = quote;
+        this.author = author;
+    }
+
+    // Setters y getters
+    public void setQuote(String quote) {
+        this.quote = quote;
+    }
+
+    public String getQuote() {
+        return quote;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void display(){
+        System.out.println("Quote: " + this.getQuote());
+        System.out.println("Author: " + this.getAuthor());
+    }
+}
